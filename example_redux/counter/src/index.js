6271b642ea9e2613c4692ae4e8d8b67ec44384a1
kenjimorita <morita_kenji_gn@cyberagent.co.jp>
2016-10-22 12:14:20 +0900

    counter init

--- example_redux/counter/src/index.js
+++ example_redux/counter/src/index.js
@@ -13,11 +13,13 @@ const store = createStore(counter)
 
 const rootEl = document.getElementById('root')
 const render = () => ReactDOM.render(
+	<div>
 	<Counter
 		value={store.getState()}
 		onIncrement={()=> store.dispatch({ type: 'INCREMENT' })}
 		onDecrement={()=> store.dispatch({ type: 'DECREMENT' })}
-	/>, rootEl)
+	/>
+	</div>, rootEl)
 
 render()
 store.subscribe(render)
