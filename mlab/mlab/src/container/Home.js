65a886593aa78cb34a9387f09070d1e5a9a8c3c5
森田賢二 <kenjimoritata@gmail.com>
2017-11-10 00:20:25 +0900

    Add

---
+++ mlab/mlab/src/container/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export class Home extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>ファファ</div>
+        )
+    }
+}
